8e2cba8b9d341df897c86c8fc482b5a59ccbef56
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-05-30 15:13:48 +0530

    adding css to admin panel.

--- custom/views.py
+++ custom/views.py
@@ -1,6 +1,8 @@
 from django.http import response
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 def home(request):
-    return render(request, 'admin/base_site.html')
+    # return render(request, 'admin/base_site.html')
+    return JsonResponse({'data': 'coming'})
