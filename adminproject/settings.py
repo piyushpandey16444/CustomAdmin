6b4cc1e18f9a4066f0ca534294eb5fff8538465c
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-05-30 14:48:23 +0530

    adding admin files to templates

--- adminproject/settings.py
+++ adminproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
 
+    'custom',
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'adminproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- custom/views.py
+++ custom/views.py
@@ -1,3 +1,6 @@
+from django.http import response
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'admin/base_site.html')
